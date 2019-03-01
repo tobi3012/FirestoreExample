ff2cb27e2db16b359d9bca6cbe3fe2811b306a04
Hieptq <tranquanghiep3012@gmail.com>
2019-03-01 15:08:16 +0700

    part7:add and retrive multiple documents

--- app/src/main/java/com/example/hieptq/firestoreexample/MainActivity.java
+++ app/src/main/java/com/example/hieptq/firestoreexample/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.hieptq.firestoreexample;
 
 import android.os.Bundle;
-import android.renderscript.Sampler;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -12,16 +11,15 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
-import com.google.firebase.firestore.SetOptions;
-
-import java.util.HashMap;
-import java.util.Map;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import javax.annotation.Nullable;
 
@@ -34,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView tvLoad;
 
     private FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
+    private CollectionReference collectionReference = firebaseFirestore.collection("Notebook");
     private DocumentReference reference = firebaseFirestore.document("Notebook/My First Note");
 
     @Override
@@ -49,7 +48,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        reference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+        /*reference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
             @Override
             public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException e) {
                 if (e != null) {
@@ -58,10 +57,30 @@ public class MainActivity extends AppCompatActivity {
                     return;
                 }
                 if (documentSnapshot.exists()) {
-                    String title = documentSnapshot.getString(KEY_TITLE);
-                    String description = documentSnapshot.getString(KEY_DESCRIPTION);
+                    Note note = documentSnapshot.toObject(Note.class);
+                    String title = note.getTitle();
+                    String description = note.getDescription();
                     tvLoad.setText("Title: " + title + "\nDescription: " + description);
                 }
+            }
+        });*/
+        collectionReference.addSnapshotListener(this, new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException e) {
+                if (e != null)
+                    return;
+                String data = "";
+                for (QueryDocumentSnapshot queryDocumentSnapshot : queryDocumentSnapshots) {
+                    Note note = queryDocumentSnapshot.toObject(Note.class);
+                    note.setDocumentId(queryDocumentSnapshot.getId());
+
+                    String documentId = note.getDocumentId();
+                    String title = note.getTitle();
+                    String description = note.getDescription();
+                    data += "ID: " + documentId
+                            + "\nTitle: " + title + "\nDescription: " + description + "\n\n";
+                }
+                tvLoad.setText(data);
             }
         });
     }
@@ -70,9 +89,11 @@ public class MainActivity extends AppCompatActivity {
         String title = etTitle.getText().toString();
         String description = etDescription.getText().toString();
 
-        Map<String, Object> note = new HashMap<>();
-        note.put(KEY_TITLE, title);
-        note.put(KEY_DESCRIPTION, description);
+//        Map<String, Object> note = new HashMap<>();
+//        note.put(KEY_TITLE, title);
+//        note.put(KEY_DESCRIPTION, description);
+
+        Note note = new Note(title, description);
 
         reference.set(note)
                 .addOnSuccessListener(new OnSuccessListener<Void>() {
@@ -91,13 +112,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void loadNote(View view) {
-        reference.get().
+        /*reference.get().
                 addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
                     @Override
                     public void onSuccess(DocumentSnapshot documentSnapshot) {
                         if (documentSnapshot != null) {
-                            String title = documentSnapshot.getString(KEY_TITLE);
-                            String description = documentSnapshot.getString(KEY_DESCRIPTION);
+                            Note note = documentSnapshot.toObject(Note.class);
+                            String title = note.getTitle();
+                            String description = note.getDescription();
                             tvLoad.setText("Title: " + title + "\nDescription: " + description);
                         } else {
                             Toast.makeText(MainActivity.this, "No Data to load!", Toast.LENGTH_SHORT).show();
@@ -110,15 +132,32 @@ public class MainActivity extends AppCompatActivity {
                         Toast.makeText(MainActivity.this, "Error!", Toast.LENGTH_SHORT).show();
                         Log.d(MainActivity.class.getSimpleName(), e.toString());
                     }
-                });
+                });*/
+        collectionReference.get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+            @Override
+            public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                String data = "";
+                for (QueryDocumentSnapshot queryDocumentSnapshot : queryDocumentSnapshots) {
+                    Note note = queryDocumentSnapshot.toObject(Note.class);
+                    note.setDocumentId(queryDocumentSnapshot.getId());
+
+                    String documentId = note.getDocumentId();
+                    String title = note.getTitle();
+                    String description = note.getDescription();
+                    data += "ID: " + documentId
+                            + "\nTitle: " + title + "\nDescription: " + description + "\n\n";
+                }
+                tvLoad.setText(data);
+            }
+        });
     }
 
     public void updateDescription(View view) {
         String description = etDescription.getText().toString();
-        Map<String, Object> note = new HashMap<>();
-        note.put(KEY_DESCRIPTION, description);
-        reference.set(note, SetOptions.merge());
-//        reference.update(KEY_DESCRIPTION, description);
+//        Map<String, Object> note = new HashMap<>();
+//        note.put(KEY_DESCRIPTION, description);
+//        reference.set(note, SetOptions.merge());
+        reference.update(KEY_DESCRIPTION, description);
     }
 
     public void deleteDescription(View view) {
@@ -131,5 +170,13 @@ public class MainActivity extends AppCompatActivity {
     public void deleteNote(View view) {
         if (reference != null)
             reference.delete();
+    }
+
+    public void addNote(View view) {
+        String title = etTitle.getText().toString();
+        String description = etDescription.getText().toString();
+
+        Note note = new Note(title, description);
+        collectionReference.add(note);
     }
 }
